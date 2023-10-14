6b7259a6908daaadc9c9eb6a43a18b13d44d3a58
edcilo <edcilop@gmail.com>
2023-10-13 22:49:19 -0600

    chore(setup): configure setup file and install application

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='fastapi',
+    version='0.1.0',
+    description='FastAPI boilerplate',
+    author='edcilo',
+    author_email='me@edcilo.com',
+    packages=['app'],
+    python_requires='>=3.11',
+    install_requires=[
+    ],
+    extras_require={
+        'dev': [],
+    },
+)
