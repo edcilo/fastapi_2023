1fe01560271a82885e710dd4ca714e39fce317a2
edcilo <edcilop@gmail.com>
2023-10-17 17:18:53 -0600

    refactor(task model): use mapped attributes

--- app/models/task.py
+++ app/models/task.py
@@ -1,9 +1,9 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 from .model import Model
 
 
 class Task(Model):
     __tablename__ = "tasks"
 
-    id = Column(Integer, primary_key=True)
-    description = Column(String(120), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    description: Mapped[str] = mapped_column(nullable=False)
